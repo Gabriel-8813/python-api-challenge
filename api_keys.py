3fafaf5732b8f91a27e81d796891db971325ee80
Gabriel Osman <gabrielosmanhamza@yahoo.com>
2024-10-13 11:43:56 -0400

    commit api keys added

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "1bd010e51d6ebffd31cb34537f5f1113"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "05d1db00fd624e94ad2b9d649ded8563"
