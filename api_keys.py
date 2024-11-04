5c22768790d505baf02c1d5ba170d0cd646bc5a0
Gabriel Osman <gabrielosmanhamza@yahoo.com>
2024-11-03 20:38:58 -0500

    commit changes

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,6 @@
+# Geoapify API Key
+geoapify_key = "434379c08c4c4db394f98b8fd2fabd03"
+# 434379c08c4c4db394f98b8fd2fabd03
+
 # OpenWeatherMap API Key
 weather_api_key = "1bd010e51d6ebffd31cb34537f5f1113"
-
-# Geoapify API Key
-geoapify_key = "05d1db00fd624e94ad2b9d649ded8563"
